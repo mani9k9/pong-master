4b1756cbc3887732dd16fc5ef8f469c837f299a5
shubham mani tripathi <42939900+mani9k@users.noreply.github.com>
2020-05-04 08:24:01 +0530

    Update Paddle.lua

--- Paddle.lua
+++ Paddle.lua
@@ -1,11 +1,5 @@
 --[[
-    GD50 2018
-    Pong Remake
-
-    -- Paddle Class --
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
+    
 
     Represents a paddle that can move up and down. Used in the main
     program to deflect the ball back toward the opponent.
